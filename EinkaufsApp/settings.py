16fcad5460194cce24fc0075b2c72704827e1909
Marius <marius000steger@gmail.com>
2020-03-18 18:40:43 +0100

    fixes

--- EinkaufsApp/settings.py
+++ EinkaufsApp/settings.py
@@ -79,6 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             'read_default_file': '/home/helfer/mysql/my.cnf',
         },
     }
