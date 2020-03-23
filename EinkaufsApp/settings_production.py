174a4911de81b200b36f6e2741fe0cf2fbd43a57
Marius <marius000steger@gmail.com>
2020-03-23 19:48:39 +0100

    test st leon unter helfer.uber.space

--- EinkaufsApp/settings_production.py
+++ EinkaufsApp/settings_production.py
@@ -38,7 +38,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hefer.uber.space', 'www.st.leon-hilft.laier-gmbh.de', 'st.leon-hilft.laier-gmbh.de']
 
 # Application definition
 
